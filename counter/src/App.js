93f17a390ae329cb520e42bc3b25b3ad04800b97
jzamora5 <jzamora_5@yahoo.com>
2021-05-27 11:21:13 -0500

    Styles sharing tests

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,16 +1,25 @@
 import { useState } from 'react';
 
 import './App.css';
+import styles from './Vars.module.css';
+
+console.log(styles);
+
+let componentVariable = '--component-variable';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState('green');
 
   const handleClick = () => {
+    if (color === 'green') setColor('red');
+    else setColor('green');
+    // debugger;
     setCount(count + 1);
   };
 
   return (
-    <div className="App">
+    <div className="App" style={{ [componentVariable]: color }}>
       <button onClick={handleClick}>#{count}</button>
     </div>
   );
